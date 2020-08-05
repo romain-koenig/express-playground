e352f1344a0c1dfe854b7cb31b87835b4caf615c
Romain KOENIG <romain.koenig@gmail.com>
2020-08-05 17:07:48 +0200

    End of reading/writing (CRUD)

---
+++ reading-writing/client-put.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const data = JSON.stringify({
+  name: 'product-updated',
+  id: 4
+})
+
+const options = {
+  hostname: 'localhost',
+  port: 4000,
+  path: '/products',
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const request = http.request(options, (res) => {
+  let body = '';
+  res.on('data', (chunk) => { body += "" + chunk; })
+  res.on('end', () => { console.log('response', body) })
+  res.on('close', () => { console.log('Closed connection') })
+})
+
+request.end(data);
