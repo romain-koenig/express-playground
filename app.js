d7545b0becd058625f5ca9ec10acdad3e6aa7558
Romain KOENIG <romain.koenig@gmail.com>
2020-08-05 17:08:12 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+let bodyParser = require('body-parser');
+
+
 const app = express();
 const port = 4000;
+app.use(bodyParser.json({ extended: false }));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/', (req, res) => res.send('Hello World!'));
-
 
 app.get("/products", (req,res) => {
   const products = [
@@ -24,7 +29,3 @@ app.get("/products", (req,res) => {
 
  res.json(products);
 });
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
